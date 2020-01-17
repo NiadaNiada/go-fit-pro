09433920755cda029cceb242b18356bfa06ccf57
Diana Arsenii <diana.arsenii@icloud.com>
2020-01-17 10:35:59 +0200

    Logo added

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,13 @@ class App extends React.Component {
   render() {
     return (
       <div>
+
         <Layout fixedHeader className="demo-big-content">
 
           <Header className="nav-bar" transparent title="GO FIT_PRO" style={{ color: "white" }} >
-            {/* <div className="logo">
-              <img clasName="logo-img" src={require('../img/logo1.png')} alt="logo" style={{ right: "100px", position: 'sticky', border: "2px solid #4CAF50" }} />
-            </div> */}
+            <i className="fab fa-gripfire"></i>
             <Navigation >
+
               <Link to="/home">Home</Link>
               <Link to="/dashboard">Dashboard</Link>
               <Link to="/workout">Workout</Link>
@@ -24,6 +24,7 @@ class App extends React.Component {
             </Navigation>
           </Header>
           <Drawer title="GO FIT_PRO" style={{ color: "black" }}>
+
             <Navigation >
               <Link to="/home">Home</Link>
               <Link to="/dashboard">Dashboard</Link>
